5d2e5f4d1e38d5584b250821d2821a13d2bc5f32
Jeff Richardson <jjrich13@gmail.com>
2018-09-20 10:51:57 -0500

    removed reference to non-existent router

--- server/server.js
+++ server/server.js
@@ -9,7 +9,6 @@ const sessionMiddleware = require('./modules/session-middleware');
 const passport = require('./strategies/github.strategy');
 
 // Route includes
-const userRouter = require('./routes/user.router');
 const challengeRouter = require('./routes/challenge.router');
 const authRouter = require('./routes/auth.router.js');
 const dashboardRouter = require('./routes/dashboard.router');
